a943534f5f8e7471127989dbf8501e1fa9ff9e89
Camilo <capinzonr@unal.edu.co>
2018-10-01 10:08:30 -0500

    Add to env pass postgres db

--- api/controllers/login.js
+++ api/controllers/login.js
@@ -5,64 +5,62 @@ const configuration = require('../../knexfile')[environment];
 const database = require('knex')(configuration);
 const bcrypt = require('bcrypt');
 const crypto = require('crypto');
-const {User} = require('../../database/models/user');
+const { User } = require('../../database/models/user');
 var util = require('util');
 var user = require('../../database/models/user');
 let userData;
 module.exports = {
-	login: login
+    login: login
 };
 
-function login(req, res)
-{
+function login(req, res) {
 
-	const form = {
-		email: req.body.email,
-		password: req.body.password
-	};
+    const form = {
+        email: req.body.email,
+        password: req.body.password
+    };
 
-	findUser(form.email)
-	.then(foundUser => {
-		userData = foundUser;
-		return checkPassword(form.password, foundUser)
-	})
-	.then((res) => createToken())
-	.then(token => updateUsertoken(token))
-	.then(() => {
-		res.status(200).send({"token" : userData[0].token})
-	})
-	.catch((e) => console.error(e));
+    findUser(form.email)
+        .then(foundUser => {
+            userData = foundUser;
+            return checkPassword(form.password, foundUser)
+        })
+        .then((res) => createToken())
+        .then(token => updateUsertoken(token))
+        .then(() => {
+            res.status(200).send({ "token": userData[0].token })
+        })
+        .catch((e) => console.error(e));
 
 }
 
-const createToken= () => {
-	return new Promise((resolve, reject) => {
-		crypto.randomBytes(16, (e, data) => {
-			e ? reject(e) : resolve(data.toString('base64'))
-		})
-	})
+const createToken = () => {
+    return new Promise((resolve, reject) => {
+        crypto.randomBytes(16, (e, data) => {
+            e ? reject(e) : resolve(data.toString('base64'))
+        })
+    })
 }
 
 const findUser = (email) => {
-	return User.query().where('email', email);
+    return User.query().where('email', email);
 };
 
 const checkPassword = (password, foundUser) => {
-	return new Promise((resolve, reject) =>
-	bcrypt.compare(password, foundUser[0].password_digest, (e, response) => {
-		if(e){
-			reject(e)
-		}
-		else if (response) {
-			resolve(response)
-		} else {
-			reject(new Error('Wrong password'))
-		}
-	})
-	)
+    return new Promise((resolve, reject) =>
+        bcrypt.compare(password, foundUser[0].password_digest, (e, response) => {
+            if (e) {
+                reject(e)
+            } else if (response) {
+                resolve(response)
+            } else {
+                reject(new Error('Wrong password'))
+            }
+        })
+    )
 };
 
 const updateUsertoken = (genToken) => {
-	userData[0].token = genToken;
-	return User.query().patch({token: genToken}).where('id', userData[0].id)
+    userData[0].token = genToken;
+    return User.query().patch({ token: genToken }).where('id', userData[0].id)
 };
--- database/models/user.js
+++ database/models/user.js
@@ -1,13 +1,8 @@
 const { Model } = require("objection");
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('../../knexfile')[environment];
-const database = require('knex')(configuration);
-const bcrypt = require('bcrypt');
-const crypto = require('crypto');
 
 class User extends Model {
 
-    static  get tableName(){
+    static get tableName() {
         return "users";
     };
 }
