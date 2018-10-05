33ff4d260de8c6ddeae05ff13a93f94d4e24601d
Camilo <capinzonr@unal.edu.co>
2018-10-05 18:23:12 -0500

    Refactor evnet.js and swagger.yaml

--- api/controllers/event.js
+++ api/controllers/event.js
@@ -4,14 +4,22 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../knexfile')[environment];
 const database = require('knex')(configuration);
 const { Event } = require('../../database/models/event');
+const knex = require('knex');
 var util = require('util');
 
 
+function getEvents(req, res) {
+    findEvents()
+        .then(events => {
+            res.status(200).send(events);
+        })
+        .catch((e) => console.error(e));
+}
+
+function findEvents() {
+    return Event.query().where(1, 1);
+}
 
 module.exports = {
-    async getPosts() {
-        const Events = await Event.find({})
-            // El sort se encaraga de ordenar los posts de más reciente a menos usando releaseDate, que es la fecha de lanzamiento de estos.
-        return Events
-    },
-};
+    getEvents
+}
