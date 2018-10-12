d3527a57045f75487d89f861d0663b21d083448a
Dago Fonseca <dagofonseca6@gmail.com>
2018-10-12 16:54:49 -0500

    getEvent validation works. hello path was deleted from swagger

--- api/controllers/event.js
+++ api/controllers/event.js
@@ -7,19 +7,36 @@ const { Event } = require('../../database/models/event');
 const knex = require('knex');
 var util = require('util');
 
-
 function getEvents(req, res) {
-    findEvents()
-        .then(events => {
-            res.status(200).send(events);
-        })
-        .catch((e) => console.error(e));
+  findEvents()
+    .then((events) => {
+      res.status(200).send(events);
+    })
+    .catch((e) => console.error(e));
 }
 
-function findEvents() {
-    return Event.query().where(1, 1);
+const findEvents = () => Event.query().where(1, 1);
+
+function getEvent(req, res) {
+  const id = req.swagger.params.id.value;
+
+  findEvent(id)
+    .then((event) => {
+      if (!event) {
+        res.status(400).send({ message: 'Invalid ID' });
+      } else {
+        res.status(200).send(event);
+      }
+    })
+    .catch((e) => console.error(e));
 }
+
+const findEvent = (id) =>
+  Event.query()
+    .where('id', id)
+    .first();
 
 module.exports = {
-    getEvents
-}
+  getEvents,
+  getEvent
+};
