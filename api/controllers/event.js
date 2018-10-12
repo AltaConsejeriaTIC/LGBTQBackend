58bba04ab7f794441daa0f4381a0d208711972e6
Camilo <capinzonr@unal.edu.co>
2018-10-12 18:05:05 -0500

    Query whit objection

--- api/controllers/event.js
+++ api/controllers/event.js
@@ -8,35 +8,35 @@ const knex = require('knex');
 var util = require('util');
 
 function getEvents(req, res) {
-  findEvents()
-    .then((events) => {
-      res.status(200).send(events);
-    })
-    .catch((e) => console.error(e));
+    findEvents()
+        .then((events) => {
+            res.status(200).send(events);
+        })
+        .catch((e) => console.error(e));
 }
 
-const findEvents = () => Event.query().where(1, 1);
+const findEvents = () => Event.query();
 
 function getEvent(req, res) {
-  const id = req.swagger.params.id.value;
+    const id = req.swagger.params.id.value;
 
-  findEvent(id)
-    .then((event) => {
-      if (!event) {
-        res.status(400).send({ message: 'Invalid ID' });
-      } else {
-        res.status(200).send(event);
-      }
-    })
-    .catch((e) => console.error(e));
+    findEvent(id)
+        .then((event) => {
+            if (!event) {
+                res.status(400).send({ message: 'Invalid ID' });
+            } else {
+                res.status(200).send(event);
+            }
+        })
+        .catch((e) => console.error(e));
 }
 
 const findEvent = (id) =>
-  Event.query()
+    Event.query()
     .where('id', id)
     .first();
 
 module.exports = {
-  getEvents,
-  getEvent
+    getEvents,
+    getEvent
 };
