2d02b45202b30b7f3dec0d3deef6ae0ed396cc31
jaavargasar <jaavargasar@unal.edu.co>
2018-10-18 15:30:16 -0500

    Added update news

--- api/controllers/news.js
+++ api/controllers/news.js
@@ -40,8 +40,45 @@ function postNews(req, res) {
 }
 const insert = (news) => News.query().insert(news);
 
+function updateNews(req, res){
+
+  const id = req.swagger.params.id.value;
+
+  findNewsId(id)
+      .then(news => {
+          if (!news) {                
+              res.status(400).send({ message: 'Invalid ID' });
+          } else {
+              newsUpdated(req.body , id)
+                .then( response => {
+                    res.status(201).send( {id: response.id} );
+                })
+                .catch((e) => console.error(e));
+
+          }
+      })
+      .catch((e) => console.error(e));
+
+}
+
+const newsUpdated = (data,id) => News.query()
+  .patchAndFetchById(id, {
+    title: data.title,
+    description: data.description,
+    source: data.source,
+    source_link: data.source_link,
+    date: data.date,
+    image_owner: data.image_owner,
+    image: data.image,
+    state: data.state,
+    created_at: data.created_at
+  });
+
+  
+
 module.exports = {
     getNews,
     getNewsId,
-    postNews
+    postNews,
+    updateNews
 };
