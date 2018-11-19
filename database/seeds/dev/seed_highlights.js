5137b32e6abf55368cf1ead018c357bfcca4e261
jaavargasar <jaavargasar@unal.edu.co>
2018-11-19 17:29:27 -0500

    Added highlight field in table name and education in user's seed

--- database/seeds/dev/seed_highlights.js
+++ database/seeds/dev/seed_highlights.js
@@ -1,12 +1,12 @@
 exports.seed = function(knex, Promise) {
     // Deletes ALL existing entries
-    return knex('table_name').del()
+    return knex('highlights').del()
         .then(function() {
             // Inserts seed entries
-            return knex('table_name').insert([
+            return knex('highlights').insert([
                 { id: 1, section_id: 4, section: 'event' },
-                { id: 1, section_id: 6, section: 'event' },
-                { id: 1, section_id: 2, section: 'news' }
+                { id: 2, section_id: 6, section: 'event' },
+                { id: 3, section_id: 2, section: 'news' }
             ]);
         });
 };
