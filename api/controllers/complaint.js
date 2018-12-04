062a9998e763ea83a1b4d9dd0e087b306a3737f1
jaavargasar <jaavargasar@unal.edu.co>
2018-12-04 17:29:07 -0500

    Added mail template in post complaints

--- api/controllers/complaint.js
+++ api/controllers/complaint.js
@@ -81,38 +81,23 @@ function postComplaint(req, res) {
               error: err
           });
         } else {
-    
-        const output = `
-          <p>Prueba aplicacion movil en bogota se puede ser</p>
-          <h3> Datos de la denuncia</h3>
-          <ul>
-            <li><b>Nombre<b>: ${data.first_name} ${data.last_name}</li>
-            <li>Tipo de documento de identidad: ${data.document_type}</li>
-            <li>Número de documento: <b>${data.document_number}</b></li>
-            <li>Correo electrónico: <b>${data.email}</b></li>
-            <li>Teléfono fijo o celular: <b>${data.phone}</b></li>
-            <li>Fecha del suceso: <b>${data.event_day}</b></li>
-            <li>Lugar del suceso: ${data.event_place}</li>
-            <li>Descripción de la situación: ${data.description}</li>
-          </ul>
-          <img src="cid:unique@kreata.ee"/>
-          
-        `
-        const mailOptions = {
-          from: 'Diversidad Sexual <diversidadsexual@sdp.gov.co>',
-          to: 'diversidadsexual@sdp.gov.co',
-          subject: 'Nodemailer test',
-          text: '',
-          html: output,
-          attachments: [{
-            filename: 'organización03',
-            path: `./public/images/organización03.JPG`,
-            cid: 'unique@kreata.ee'
-        }]
-        }
-      
+  
         insert(data)
           .then(response => {
+            
+            const mailOptions = {
+              from: 'Diversidad Sexual <diversidadsexual@sdp.gov.co>',
+              to: 'diversidadsexual@sdp.gov.co',
+              subject: 'Nodemailer test',
+              text: '',
+              html: getHtmlDesign(data, new Date(), response.id ),
+              attachments: [{
+                filename: 'organización03',
+                path: `./public/images/organización03.JPG`,
+                cid: 'unique@kreata.ee'
+            }]
+            }
+
             transporter.sendMail(mailOptions, function (err, res) { 
               if(err){
                   console.log('Error sending email');
@@ -184,3 +169,135 @@ module.exports = {
     postComplaint,
     updateComplaint
 };
+
+function getHtmlDesign( data, date , id ){
+
+   const output = `
+    <html>
+
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <link href="https://fonts.googleapis.com/css?family=Poppins|Raleway" rel="stylesheet">
+
+        <style type="text/css">
+            .template-mail {
+                margin-left: 3%;
+                margin-right: 3%;
+            }
+
+            .template-mail .section-body .count-box{
+              margin-top: 0%;
+              margin-bottom: 0%;
+            }
+            
+            .template-mail .section-body .change-bold{
+              font-weight: bold;
+            }
+
+            .mt-0{
+              margin-top: 0%;
+            }
+
+            .mb-0{
+              margin-bottom: 0%;
+            }
+
+            .template-mail .section-body .box-text-flex .title-complaint{
+              text-decoration: underline;
+            }
+
+            .template-mail .section-body .description-text{
+              text-align: justify;
+              text-justify: inter-word;
+            }
+
+            .template-mail .section-body .color-gray{
+              color: rgb(102, 102, 102);
+            }
+
+            .template-mail .section-body .box-text-flex{
+              display: flex;
+              flex-direction: row;
+            }
+
+            .complaint-title-size{
+              font-size: 1.8vh;
+            }
+
+            .ml-set{
+              margin-left: 1%;
+            }
+
+            .blue-color{
+              color: #15c;
+            }
+
+            .mt-n{
+              margin-top: 1%;
+
+            }
+
+            .to-uppercase{
+              text-transform: uppercase;
+            }
+
+            .to-italic{
+              font-style: italic;
+            }
+
+        </style>
+    </head>
+
+    <body>
+        <div class="template-mail">
+
+            <!--section-body-->
+            <div class="section-header">
+
+
+            </div>
+            <!--end section-body-->
+
+            <!--section-body-->
+            <div class="section-body">
+
+              <p class="count-box color-gray to-italic">Respuesta Nº <b>${id}</b> formulario de denuncia </p>
+              <p class="count-box color-gray"><b>Dirección de Diversidad Sexual</b></p>
+              <p class="count-box color-gray"><b>App En Bogotá Se Puede Ser</b></p>
+
+              <div class="box-text-flex mt-n">
+                  <h1 class="mb-0 complaint-title-size">DENUNCIA DE:</h1> 
+                  <h1 class="title-complaint mb-0 complaint-title-size ml-set to-uppercase"> ${data.first_name} ${data.last_name}</h1>
+              </div>
+              
+              <p class="mt-0 color-gray">Fecha de envío: ${date.getFullYear()}-${date.getMonth()}-${date.getDay()} </p>
+
+              <p class="mb-0"><b>Nombres y apellidos:</b> ${data.first_name} ${data.last_name}</p>
+              <p class="mb-0 mt-0"><b>Tipo de documento de identidad:</b> ${data.document_type}</p>
+              <p class="mb-0 mt-0"><b>Número de documento:</b> ${data.document_number}</p>
+              <div class="box-text-flex">
+                  <p class="mb-0 mt-0"><b>Correo electrónico:</b> </p>
+                  <p class="mb-0 mt-0 title-complaint blue-color ml-set"> <b> ${data.email}</b></p>
+              </div>
+              <p class="mb-0 mt-0"><b>Teléfono fijo o celular:</b> ${data.phone}</p>
+              <p class="mb-0 mt-0"><b>Fecha del suceso:</b> ${data.event_day}</p>
+              <p class="mb-0 mt-0"><b>Lugar del suceso:</b> ${data.event_place}</p>
+              <p class="mb-0"><b>Descripción de la situación: </b> </p>
+              <p class="mt-0 description-text"> ${data.description}</p>
+
+            </div>
+            <!--end section-body-->
+
+            <!--section-footer-->
+            <div class="section-footer">
+
+            </div>
+            <!--end section-footer-->
+
+        </div>
+    </body>
+
+    </html>`;
+
+  return output;
+}
