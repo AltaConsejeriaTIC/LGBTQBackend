2c9cc50112a8d0b14cf126edef95ba924494f739
capinzor <capinzonr@unal.edu.co>
2018-12-09 23:25:27 -0500

    validations at events

--- api/controllers/event.js
+++ api/controllers/event.js
@@ -7,6 +7,25 @@ const { Event } = require('../../database/models/event');
 const knex = require('knex');
 var util = require('util');
 const AdminHelper = require('../helpers/admin_helper');
+const Joi = require('joi');
+
+const schema = Joi.object().keys({
+
+    title: Joi.string().max(45).required(),
+    description: Joi.string().min(150).max(800).required(),
+    place: Joi.string(),
+    address: Joi.string().required(),
+    start_date: Joi.date().required(),
+    finish_date: Joi.date().required(),
+    start_time: Joi.string().required(),
+    finish_time: Joi.string().required(),
+    image: Joi.string().required(),
+    state: Joi.boolean().default(true),
+    latitude: Joi.number().required(),
+    longitude: Joi.number().required(),
+
+
+});
 
 function getEvents(req, res) {
     findEvents()
@@ -47,19 +66,33 @@ const findEvent = (id) => Event.query().where('id', id).first();
 function postEvent(req, res) {
 
     const token = req.headers.token;
-    AdminHelper.isAuthenticate(token)
-        .then((dataAdmin) => {
-            if (dataAdmin.length === 1) {
-                insert(req.body)
-                    .then(response => {
-                        res.status(201).send({ id: response.id });
-                    })
-                    .catch(e => console.error(e));
-            } else {
-                res.status(403).send({ message: 'Forbidden permissions' });
-            }
-        })
-        .catch(e => console.error(e));
+    const data = req.body;
+
+    Joi.validate(data, schema, (err, value) => {
+
+      if (err) {
+          res.status(422).json({
+              status: 'error',
+              message: 'Invalid request data',
+              error: err
+          });
+      } else {
+          AdminHelper.isAuthenticate(token)
+              .then((dataAdmin) => {
+                  if (dataAdmin.length === 1) {
+                      insert(req.body)
+                          .then(response => {
+                              res.status(201).send({ id: response.id });
+                          })
+                          .catch(e => console.error(e));
+                  } else {
+                      res.status(403).send({ message: 'Forbidden permissions' });
+                  }
+              })
+              .catch(e => console.error(e));
+      }
+
+  });
 }
 
 const insert = (event) => Event.query().insert(event);
@@ -96,31 +129,42 @@ function updateStateEvent(req, res) {
 
 function updateEvent(req, res) {
 
-    const id = req.swagger.params.id.value;
-    const token = req.headers.token;
+  const id = req.swagger.params.id.value;
+  const data = req.body;
+  const token = req.headers.token;
+  Joi.validate(data, schema, (err, value) => {
 
-    AdminHelper.isAuthenticate(token)
-        .then((dataAdmin) => {
-            if (dataAdmin.length === 1) {
-                findEvent(id)
-                    .then(event => {
-                        if (!event) {
-                            res.status(400).send({ message: 'Invalid ID' });
-                        } else {
-                            eventUpdated(req.body, id)
-                                .then(response => {
-                                    res.status(201).send({ id: response.id });
-                                })
-                                .catch((e) => console.error(e));
+      if (err) {
+          res.status(422).json({
+              status: 'error',
+              message: 'Invalid request data',
+              error: err
+          });
+      } else {
+          AdminHelper.isAuthenticate(token)
+              .then((dataAdmin) => {
+                  if (dataAdmin.length === 1) {
+                      findEvent(id)
+                          .then(event => {
+                              if (!event) {
+                                  res.status(400).send({ message: 'Invalid ID' });
+                              } else {
+                                  eventUpdated(req.body, id)
+                                      .then(response => {
+                                          res.status(201).send({ id: response.id });
+                                      })
+                                      .catch((e) => console.error(e));
 
-                }
-            })
-            .catch((e) => console.error(e));
-        }else{
-          res.status(403).send({ message: 'Forbidden permissions' });
-        }
-      })
-      .catch(e => console.error(e));
+                              }
+                          })
+                          .catch((e) => console.error(e));
+                  } else {
+                      res.status(403).send({ message: 'Forbidden permissions' });
+                  }
+              })
+              .catch(e => console.error(e));
+      }
+  });
 }
 
 const eventUpdated = (data, id) => Event.query()
