51a9621b0a5705e48de56154127db6ef816a689a
capinzor <capinzonr@unal.edu.co>
2018-12-10 01:30:28 -0500

    Adding validations to news

--- api/controllers/news.js
+++ api/controllers/news.js
@@ -2,6 +2,21 @@
 
 const { News } = require('../../database/models/news');
 const  AdminHelper = require('../helpers/admin_helper');
+const Joi = require('joi');
+
+const schema = Joi.object().keys({
+
+    title: Joi.string().max(82).required(),
+    description: Joi.string().max(1000).required(),
+    source: Joi.string(),
+    source_link: Joi.string(),
+    date: Joi.date().required(),
+    image_owner: Joi.string(),
+    image: Joi.string().required(),
+    state: Joi.boolean().default(true)
+
+
+});
 
 
 function getNews(req, res) {
@@ -51,20 +66,34 @@ const findNewsId = (id) => News.query().where('id', id).first();
 
 function postNews(req, res) {
 
-    const token = req.headers.token;
-    AdminHelper.isAuthenticate(token)
-    .then( (dataAdmin)=>{
-      if( dataAdmin.length === 1 ){
-        insert(req.body)
-        .then(response => {
-            res.status(201).send({ id: response.id });
-        })
-        .catch(e => console.error(e));
-      }else{
-        res.status(403).send({ message: 'Forbidden permissions' });
-      }
-    })
-    .catch(e => console.error(e));
+  const token = req.headers.token;
+  const data = req.body;
+
+  Joi.validate(data, schema, (err, value) => {
+
+    if (err) {
+        res.status(422).json({
+            status: 'error',
+            message: 'Invalid request data',
+            error: err
+        });
+    } else {
+        AdminHelper.isAuthenticate(token)
+            .then((dataAdmin) => {
+                if (dataAdmin.length === 1) {
+                    insert(req.body)
+                        .then(response => {
+                            res.status(201).send({ id: response.id });
+                        })
+                        .catch(e => console.error(e));
+                } else {
+                    res.status(403).send({ message: 'Forbidden permissions' });
+                }
+            })
+            .catch(e => console.error(e));
+    }
+
+  });
 
     
 }
@@ -72,31 +101,42 @@ const insert = (news) => News.query().insert(news);
 
 function updateNews(req, res) {
 
-    const id = req.swagger.params.id.value;
-    const token = req.headers.token;
-    AdminHelper.isAuthenticate(token)
-    .then( (dataAdmin)=>{
-      if( dataAdmin.length === 1 ){
-        findNewsId(id)
-        .then(news => {
-            if (!news) {
-                res.status(400).send({ message: 'Invalid ID' });
-            } else {
-                newsUpdated(req.body, id)
-                    .then(response => {
-                        res.status(201).send({ id: response.id });
-                    })
-                    .catch((e) => console.error(e));
-            }
-        })
-        .catch((e) => console.error(e));
-      }else{
-        res.status(403).send({ message: 'Forbidden permissions' });
-      }
-    })
-    .catch(e => console.error(e));
+  const id = req.swagger.params.id.value;
+  const data = req.body;
+  const token = req.headers.token;
+  Joi.validate(data, schema, (err, value) => {
 
-    
+      if (err) {
+          res.status(422).json({
+              status: 'error',
+              message: 'Invalid request data',
+              error: err
+          });
+      } else {
+          AdminHelper.isAuthenticate(token)
+              .then((dataAdmin) => {
+                  if (dataAdmin.length === 1) {
+                    findNewsId(id)
+                          .then(news => {
+                              if (!news) {
+                                  res.status(400).send({ message: 'Invalid ID' });
+                              } else {
+                                  newsUpdated(req.body, id)
+                                      .then(response => {
+                                          res.status(201).send({ id: response.id });
+                                      })
+                                      .catch((e) => console.error(e));
+
+                              }
+                          })
+                          .catch((e) => console.error(e));
+                  } else {
+                      res.status(403).send({ message: 'Forbidden permissions' });
+                  }
+              })
+              .catch(e => console.error(e));
+      }
+  }); 
 
 }
 
