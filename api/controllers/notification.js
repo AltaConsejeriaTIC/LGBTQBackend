f622b67ca3c5c8044f460099591196f1e05494d6
Fabian Novoa <fabianovoa@seven4n.com>
2018-12-13 00:15:19 -0500

    New notification service, and add token service

--- api/controllers/notification.js
+++ api/controllers/notification.js
@@ -1,6 +1,7 @@
 'use strict';
 const serverKey ='AAAAo2sq-a0:APA91bHGdvVYMnXJf0EvTSdcmpJ0SwHPjc9u7asFN_1_d8eKY092PGJBEmRTOCn7y8z8w_3uvsMZ6z3zLVgdF07Mtib5tbJUcGMO2pMTmN8BEWqGah8Hml35nNhL9m94gHkKT-aei3gQ';
 const FCM = require('fcm-node');
+const { Token } = require("../../database/models/token");
 
 module.exports = {
   addToken: addToken,
@@ -9,30 +10,39 @@ module.exports = {
 };
 
 function addToken(req, res) {
-  // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
-  var token = req.swagger.params.token;
-  console.log(token);
-  // this sends back a JSON response which is a single string
-  res.json({token:token.value});
+  const token = req.swagger.params.token;
+
+  Token.query().insert({token:token.value})
+    .then(response => {
+      console.log("Token Registered"+ token );
+      res.status(201).send({ token: token });
+    })
+    .catch(e => {
+      console.error(e);
+      res.json({token:'ERROR'});
+    });
 }
 
 function send(req, res) {
   let fcm = new FCM(serverKey);
+  Token.query().then((tokens) => {
+    tokens.forEach(function (element) {
+      const message = {
+        to: element.token,
+        notification: {
+          title: req.body.title,
+          body: req.body.body
+        }
+      };
+      fcm.send(message, function (err) {
+        if (err) {
+          console.log("Error sending notification to ", element.id)
+        } else {
+          console.log("Success sending notification to ", element.id)
+        }
+      });
+    });
+  }).catch((e) => console.error(e));
 
-  const message = {
-    to: 'exbQVqmheIg:APA91bHeiqReYPy41pe5Q5SPUwOPxUX51vNUexLThVC5LrVyFSHgFDcjwWYJuK24sR9OUCISM0xcLtp5p64EsnxKKvsOFboWsp3_Wf5-NNuwVcO6kb5NbXIuXtXpvcWs-6utgKEvnPHM',
-    notification: {
-      title: req.body.title,
-      body: req.body.body
-    }
-  };
-
-  fcm.send(message, function(err){
-    if (err) {
-      res.json({result: 'ERROR'});
-    } else {
-      res.json({result:'OK'});
-    }
-  });
-
+  res.json({result: 'NOTIFICATION PROCESS HAS STARTED'});
 }
--- database/migrations/20181212221040_create_token_table.js
+++ database/migrations/20181212221040_create_token_table.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('tokens', table => {
       table.increments('id').primary();
-      table.string('token');
+      table.string('token',500);
       table.timestamp('created_at').defaultTo(knex.fn.now());
     })
   ]);
