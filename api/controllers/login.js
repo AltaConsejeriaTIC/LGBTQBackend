d584353156c9db6dedbf368f5a1ef2375d0faa0b
jaavargasar <jaavargasar@unal.edu.co>
2018-12-12 19:27:16 -0500

    Added log out method in login controller

--- api/controllers/login.js
+++ api/controllers/login.js
@@ -8,9 +8,11 @@ const crypto = require('crypto');
 const { Admin } = require('../../database/models/admin');
 var util = require('util');
 var admin = require('../../database/models/admin');
+const AdminHelper = require('../helpers/admin_helper');
 let adminData;
 module.exports = {
-    login: login
+    login: login,
+    deleteAdminToken
 };
 
 function login(req, res) {
@@ -27,10 +29,46 @@ function login(req, res) {
         .then((res) => createToken())
         .then((token) => updateAdmintoken(token))
         .then(() => {
-            res.status(200).send({ token: adminData[0].token });
+            res.status(200).send({ token: adminData[0].token, id: adminData[0].id  });
         })
         .catch((e) => console.error(e));
 }
+
+
+function deleteAdminToken(req, res){
+  const id = req.swagger.params.id.value;
+  const token = req.headers.token;
+
+  AdminHelper.isAuthenticate(token)
+      .then((dataAdmin) => {
+          if (dataAdmin.length === 1) {
+            findAdminById(id)
+                  .then(admin => {
+                      if( admin.length === 1){
+                        tokenDelete( id )
+                              .then(response => {
+                                  res.status(200).send({ message: 'Token has been deleted successfully' });
+                              })
+                              .catch((e) => console.error(e));
+                      }
+                      else{
+                        res.status(400).send({ message: 'Invalid ID' });
+                      }
+                  })
+                  .catch((e) => console.error(e));
+          } else {
+              res.status(403).send({ message: 'Forbidden permissions' });
+          }
+      })
+      .catch(e => console.error(e));
+}
+
+const findAdminById = (id) => Admin.query().where('id', id);
+
+const tokenDelete = ( id ) => Admin.query()
+    .patchAndFetchById(id, {
+        token: null
+    });
 
 const createToken = () => {
     return new Promise((resolve, reject) => {
