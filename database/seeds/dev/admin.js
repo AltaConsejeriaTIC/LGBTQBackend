8aeff66d51044de427cab9f1b60e0bbe97946335
jaavargasar <jaavargasar@unal.edu.co>
2018-12-28 13:25:56 -0500

    now it only has 2 admins

--- database/seeds/dev/admin.js
+++ database/seeds/dev/admin.js
@@ -6,14 +6,10 @@ exports.seed = function(knex, Promise) {
     return knex('admins').del()
         .then(function() {
             hashedPassword = bcrypt.hashSync('acuerdo371', 10);
+            hashedPassword2 = bcrypt.hashSync('371acuerdo', 10);
             return knex('admins').insert([
                 { email: 'diversidadsexual@sdp.gov.co', password_digest: hashedPassword },
-                { email: 'vivelabbogota@gmail.com', password_digest: hashedPassword },
-                { email: 'laura@gmail.com', password_digest: hashedPassword },
-                { email: 'brigitte@gmail.com', password_digest: hashedPassword },
-                { email: 'dds@gmail.com', password_digest: hashedPassword },
-                { email: 'test@gmail.com', password_digest: hashedPassword },
-                { email: 'something@gmail.com', password_digest: hashedPassword }
+                { email: 'diversidadsexual1@sdp.gov.co', password_digest: hashedPassword2 }
             ]);
         });
 };
